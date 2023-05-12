a11284aeac901ee69c6a74af2e3141c15b10d36e
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-05-12 09:24:31 +0600

    Connect Admin Panel with Blog Table of Database

--- Create-Project/Admin/Admin-Panel.aspx.cs
+++ Create-Project/Admin/Admin-Panel.aspx.cs
@@ -5,13 +5,38 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;   
+
 namespace Create_Project.Admin
 {
     public partial class Admin_Panel : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!Page.IsPostBack)
+            {
+                LabelBPosteddate.Text = DateTime.Now.ToString();
+                DDLBCat.Items.Insert(0,"--Select Category--");
+            }
+        }
 
+        protected void ButSubmit_Click(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "insert into [dbo].[blog] (Btitle,Bcatagory,BDesc,BUrl,Bposteddate) values (@Btitle,@Bcatagory,@BDesc,@BUrl,@Bposteddate)";
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("@Btitle",TxtBlogTitle.Text);
+            sqlcomm.Parameters.AddWithValue("@Bcatagory", DDLBCat.SelectedItem.Text.ToString());
+            sqlcomm.Parameters.AddWithValue("@BDesc", TxtBDesc.Text);
+            sqlcomm.Parameters.AddWithValue("@BUrl", TxtBurl.Text);
+            sqlcomm.Parameters.AddWithValue("@Bposteddate", LabelBPosteddate.Text);
+            sqlcomm.ExecuteNonQuery();
+            sqlconn.Close();
+            Response.Redirect("~/Admin/Admin-Panel.aspx");
         }
     }
 }
